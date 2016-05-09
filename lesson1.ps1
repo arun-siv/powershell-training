d8454dfa4ee91cc8b29883d8131d9a75f00cf141
Arun <arun.siv81@gmail.com>
2016-05-09 14:20:29 -0700

    lesson 1

---
+++ lesson1.ps1
@@ -0,0 +1,11 @@
+﻿#variables
+$x  = 10
+$x.GetType() #  should return integer
+
+
+#DIFFERENCE BTW WRITE-OUTPUT AND WRITE HOST
+#WRITE-OUTPUT OUTPUTS THE OBJECT INTO THE CONSOLE
+Write-Output "hello" | where {$_.length -ge 4 }
+
+Write-Output 1,2,3 | where { $_ -gt 2 }
+Write-host 1,2,3 | where { $_ -gt 2 }
