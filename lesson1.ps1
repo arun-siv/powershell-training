f13d6ab43da6ad303fdaea5f15982b98542b9dbd
Arun <arun.siv81@gmail.com>
2016-05-09 14:24:28 -0700

    lesson 1

--- lesson1.ps1
+++ lesson1.ps1
@@ -9,3 +9,13 @@ Write-Output "hello" | where {$_.length -ge 4 }
 
 Write-Output 1,2,3 | where { $_ -gt 2 }
 Write-host 1,2,3 | where { $_ -gt 2 }
+
+#A simple script
+write-host "Enter your name:"
+$name = Read-Host
+write-host "enter the first number:"
+[int]$x = Read-Host
+write-host "enter the second number:"
+[int]$y = Read-Host
+$a = $x + $y
+write-host "Welcome to Powershell $name  , the sum of numbers you entered is $a"
