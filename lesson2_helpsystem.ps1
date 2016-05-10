79d407295d9862ce67186de4eeee3b902fab9b1a
Arun <arun.siv81@gmail.com>
2016-05-10 14:06:29 -0700

    new files

---
+++ lesson2_helpsystem.ps1
@@ -0,0 +1,17 @@
+﻿#understanding the help system
+update-help -Force
+
+#help <cmdlet>
+
+#help *partial*
+
+#help <verb/noun>
+
+#help <cmdlet> -full
+
+#help <cmdlet> -online
+
+#help <cmdlet> -showWindow
+
+#help about_*
+ Get-Command -Verb * | Measure-Object
---
+++ lesson3_pipeline.ps1
@@ -0,0 +1,9 @@
+﻿@"
+OBJECTS OF POWERSHELL ARE FILTERABLE , SORTABLE  , FILTERABLE AND FORMATABLE
+get-service | select name , status
+
+we also studied about where , select , format , and how to include custom properties
+"@
+
+Get-Service | where {$_.status -eq "Running" } |
+ select name , status, @{n="DisplayName";e={$_.name.toUpper()}} -First 3 | ft -AutoSize
