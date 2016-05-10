95a183a1e96fc1717c9c3539f69f4d812bc19fc9
Arun <arun.siv81@gmail.com>
2016-05-10 16:10:06 -0700

    new files

---
+++ lesson4_comparison.ps1
@@ -0,0 +1,17 @@
+﻿#replace operator is used in strings
+
+$a = @"
+the quick brown fox jumps over
+the lazy dog
+"@
+
+$a -replace "FOX" , "dog"
+
+
+#contain is used in array
+
+$a = 1, 3 ,4 
+$a -contains 1
+
+#other operators
+# -eq , -ge , -gt , -le , lt , -or , -and 
---
+++ lesson5_loops.ps1
@@ -0,0 +1,35 @@
+﻿#do loop
+
+$i = 1 
+Do {
+ "Powershell is Great! $i"
+ $i++
+
+
+} while ($i -le 5 ) #also do-untils
+
+
+#while
+$i = 5
+while ($i -ge 1) {
+" Scripting is great! $i"
+$i--
+}
+
+
+#foreach
+1..5 | foreach { Start-Process calc }
+
+#logical construct 
+ if ($x -lt $y)
+ {
+    #command 
+ }
+ elseif ($a -gt $b)
+ {
+   #command  
+ } else {
+  
+ }
+
+ 
