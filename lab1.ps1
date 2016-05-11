a41952dbee6f5953b4ffd450876108db517e26cd
Arun <arun.siv81@gmail.com>
2016-05-10 17:47:17 -0700

    changes

---
+++ lab1.ps1
@@ -0,0 +1,26 @@
+﻿$path = 'C:\Users\taiindia\powershell-training'
+$file = 'serverlist.txt'
+
+$fullpath = Join-Path -Path $path -ChildPath $file
+
+if (test-path $path\prod.csv) { Clear-Item $path\prod.csv } 
+if (test-path $path\dev.csv) { Clear-Item $path\dev.csv }
+if (test-path $path\test.csv) { Clear-Item $path\test.csv }
+
+
+Get-Content $fullpath |  foreach { 
+
+if ( $_ -like '*prod*' ) {
+         
+         $_ | Out-File -FilePath $path\prod.csv -Append
+
+    } elseif ($_ -like '*dev*') { 
+        $_ | out-file -FilePath $path\dev.csv -Append
+      
+    } else {
+
+      $_ | Out-File -FilePath $path\test.csv -Append
+    }
+  
+    
+}
---
+++ lab2.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host " Enter the operator" 
+
+$op = read-host
+
+$x = 10 
+
+$y = 30
+
+switch ($op ) {
+
+'*'  { "Output is $($x * $y)" } 
+'+'  { "Output is $($x + $y)" }
+'-'  { "output is $($y - $x)" }
+'/'  { "Output is $([math]::round($x/$y , 2))" }
+
+     default { " Exiting the code"  } 
+}
